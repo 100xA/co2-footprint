0d23a895eb2c3974547a2b35c20f59787e8193d8
JanAppAkademie <jan.biernacki@app-akademie.com>
2024-02-02 21:16:10 +0100

    First Commit + Hosting Github Pages

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Table } from './components/Table';
+
+function App() {
+  return (
+   <><Navbar /><Table/><Footer /></>
+  );
+}
+
+export default App;
